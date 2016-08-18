f876ee39c82c1fbcc6ebf0364e7148d43d213f2f
Hannah Newsome <h.e.newsome@gmail.com>
2016-08-17 23:08:08 -0700

    Initial Commit

---
+++ Bulls and Cows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bulls and Cows
+//
+//  Created by Hannah E Newsome on 8/17/16.
+//  Copyright © 2016 Hannah E Newsome. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
